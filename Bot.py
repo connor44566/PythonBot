3b037a68a2b48dadc233dd8068ec0464286964a3
connor44566 <connor68989@gmail.com>
2013-06-19 11:56:18 +0200

    Bot edit

--- Bot.py
+++ Bot.py
@@ -5,8 +5,8 @@ print("Welcome to System Security Inc.")
 print("-- where security is our middle name\n")
 
 password = input("Enter your password: ")
-
-if password == "secret":
+#Enter password in if password =="Enter password here"
+if password == "Enter password here":
     print("Access Granted")
 
 input("\n\nPress the enter key to exit.")
